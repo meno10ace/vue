0b1ea202ef0eb0992874c08cb5ce09a471e16171
fuguo <meno10.ace@gmail.com>
2024-06-26 17:19:13 +0900

    get tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-// namespace Database\Seeders;
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 // use TaskTableSeeder;
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        // $this->call(TasksTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
     }
 }
